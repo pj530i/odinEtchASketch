67f4d756096bfddd4af3bf6cd4c009ded6f6114d
Paul McAllister <360455+pj530i@users.noreply.github.com>
2022-08-01 23:59:33 -0400

    Project completed.  Page defaults to 16x16 grid, user can click the resize button to enter a new value between 1 and 100 inclusive to reset/resize.  Added a second button to toggle between drawing with black pixels and drawing with random colored pixels.

--- code.js
+++ code.js
@@ -1,5 +1,61 @@
-function dogs() {
-    alert("dog");
+
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
-dogs();
+const gridLimit = 100;
+const defaultGridSize = 16;
+
+function initButtons() {
+    const resetBtn = document.querySelector('#resize');
+    resetBtn.addEventListener('click', () => {
+        let newSize = parseInt(prompt("Enter a grid size between 1 and 100"));
+        if(newSize && newSize > 0 && newSize < gridLimit + 1 ) {
+            createGrid(newSize);
+        }
+    });
+
+    const toggleBtn = document.querySelector('#toggle');
+    toggleBtn.addEventListener('click', () => {
+        if(toggleBtn.classList.contains('rainbow')){
+            toggleBtn.classList.remove('rainbow');
+        } else {
+            toggleBtn.classList.add('rainbow');
+        }
+    });
+}
+
+function createGrid(gridSize) {
+    const container = document.querySelector('#gridContainer');
+
+    removeAllChildNodes(container);
+
+    for(let y = 0; y < gridSize; y++)
+    {
+        const newRow = document.createElement('div');
+        newRow.classList += 'row';
+        
+        for(let x = 0; x < gridSize; x++)
+        {
+            const newBlock = document.createElement('div');
+            newBlock.classList += 'block';
+            newBlock.addEventListener('mouseover', (event) => {
+                const toggleBtn = document.querySelector('#toggle');
+                if(toggleBtn.classList.contains('rainbow')) {
+                // Random color
+                    newBlock.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+                } else {
+                    newBlock.style.backgroundColor = 'black';
+                }
+            });
+            newRow.appendChild(newBlock);
+        }
+        container.appendChild(newRow);
+    }
+}
+
+initButtons();
+createGrid(defaultGridSize);
